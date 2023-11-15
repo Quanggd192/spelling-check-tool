260b564ca72f0e78af5c09aa8767412a91b15e38
Leo Nguyen <quang.nguyen@savvycomsoftware.com>
2023-11-15 11:10:41 +0700

    remove env

--- src/pages/OpenAI.jsx
+++ src/pages/OpenAI.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import OpenAI from "openai";
 
+const prePrompt =
+  "Suppose you are a university lecturer and are editing a list of articles for students in the following array, please help me edit it in a better and more optimal way if possible.";
 const ChatbotApp = () => {
   // const configuration = new Configuration({
   //   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
@@ -9,21 +11,27 @@ const ChatbotApp = () => {
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
   });
+  console.log(
+    "process.env.REACT_APP_OPENAI_API_KEY",
+    process.env.REACT_APP_OPENAI_API_KEY
+  );
   // const openai = new OpenAIApi(configuration);
-  const [prompt, setPrompt] = useState("");
+  const [textInput, setTextInput] = useState("");
   const [apiResponse, setApiResponse] = useState("");
+  const [listEssays, setListEssays] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  console.log("listEssays", listEssays);
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
+      const prompt = `${prePrompt} ${listEssays}`;
       const result = await openai.chat.completions.create({
         messages: [{ role: "system", content: prompt }],
         model: "gpt-3.5-turbo",
       });
       //console.log("response", result.data.choices[0].text);
-      console.log('result', result)
+      console.log("result", result);
       setApiResponse(result.choices[0].message.content);
     } catch (e) {
       //console.log(e);
@@ -32,37 +40,63 @@ const ChatbotApp = () => {
     setLoading(false);
   };
 
-  console.log('apiResponse', apiResponse)
+  const onAddEssay = () => {
+    const newListEssays = [...listEssays];
+    newListEssays.push(textInput);
+    console.log("newListEssays", newListEssays);
+    setListEssays(newListEssays);
+    setTextInput("");
+  };
+  console.log("apiResponse", apiResponse);
   return (
     <>
       <div
         style={{
-          marginTop: "100px",
+          margin: "auto",
+          paddingTop: "100px",
           height: "100vh",
+          width: "400px",
         }}
       >
-        <form
-          onSubmit={handleSubmit}
-          style={{ width: "400px", margin: "auto", marginTop: "100px" }}
-        >
+        <div>
           <div>
             <textarea
               style={{ width: "400px" }}
               type="text"
-              value={prompt}
+              value={textInput}
               placeholder="Please ask to openai"
-              onChange={(e) => setPrompt(e.target.value)}
+              onChange={(e) => setTextInput(e.target.value)}
               rows={5}
             ></textarea>
           </div>
           <div>
-            <button disabled={loading || prompt.length === 0} type="submit">
+            {/* <button disabled={loading || prompt.length === 0} type="submit">
+              {loading ? "Generating..." : "Generate"}
+            </button> */}
+            <button onClick={onAddEssay}>Add Essay</button>
+            <br />
+            <button
+              disabled={loading || listEssays.length === 0}
+              onClick={handleSubmit}
+            >
               {loading ? "Generating..." : "Generate"}
             </button>
           </div>
-        </form>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div>
+          <label>List Essays: </label>
+          {listEssays.map((item, index) => (
+            <div key={`essay-${index}`}>
+              <br />
+              {item}
+            </div>
+          ))}
+        </div>
       </div>
-      {apiResponse && (
+      {/* {apiResponse && (
         <div
           style={{
             display: "flex",
@@ -74,7 +108,7 @@ const ChatbotApp = () => {
             {apiResponse}
           </pre>
         </div>
-      )}
+      )} */}
     </>
   );
 };
